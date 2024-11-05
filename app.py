2eca4f368a2e8687708ebc896e709c317acb36a8
Daniil(coldwatd) <daniil@mail.com>
2024-11-05 08:31:49 +0700

    add folders and support many files

--- app.py
+++ app.py
@@ -1,27 +1,24 @@
-from flask import Flask, render_template, request, send_file, redirect, url_for
+from flask import Flask, render_template, request, send_file
 import requests
 
 app = Flask(__name__)
 YANDEX_API_URL = "https://cloud-api.yandex.net/v1/disk/public/resources"
 
-# Получение списка файлов
-def get_files(public_key):
-    params = {"public_key": public_key, "limit": 100}  # Устанавливаем лимит на 100 файлов за запрос
+# Функция для получения файлов и папок
+def get_files(public_key, path=''):
+    params = {"public_key": public_key, "path": path, "limit": 100}
     files = []
-    while True:
-        response = requests.get(YANDEX_API_URL, params=params)
-        if response.status_code == 200:
-            items = response.json().get("_embedded", {}).get("items", [])
-            files.extend([{"name": item["name"], "file": item["file"]} for item in items if item["type"] == "file"])
-            # Проверяем, есть ли еще файлы для загрузки
-            if len(items) < params["limit"]:
-                break
-            # Увеличиваем offset для следующей порции файлов
-            params["offset"] = params.get("offset", 0) + params["limit"]
-        else:
-            break
-    return files if files else None
-
+    response = requests.get(YANDEX_API_URL, params=params)
+    if response.status_code == 200:
+        items = response.json().get("_embedded", {}).get("items", [])
+        for item in items:
+            if item["type"] == "dir":
+                # Если элемент - папка, добавляем её с пустым списком вложенных файлов
+                files.append({"name": item["name"], "type": "folder", "path": item["path"], "items": []})
+            elif item["type"] == "file":
+                # Если элемент - файл, добавляем ссылку для скачивания
+                files.append({"name": item["name"], "type": "file", "file": item["file"]})
+    return files
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -34,11 +31,14 @@ def index():
 @app.route('/download', methods=['POST'])
 def download():
     file_url = request.form['file_url']
+    parsed_url = urlparse(file_url)
+    filename = parse_qs(parsed_url.query).get('filename', ['downloaded_file'])[0]
+    
     response = requests.get(file_url, stream=True)
-    filename = file_url.split('/')[-1]
     with open(filename, 'wb') as f:
         for chunk in response.iter_content(8192):
             f.write(chunk)
+    
     return send_file(filename, as_attachment=True)
 
 if __name__ == '__main__':
